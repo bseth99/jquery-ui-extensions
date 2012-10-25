a57460d9d82e5f434ac2699e15e54517b0a7a69f
Ben Olson <bseth99@yahoo.com>
2012-10-25 15:35:47 -0400

    Grab new versions

---
+++ update.sh
@@ -0,0 +1,2 @@
+wget -O ui/jquery.ui.spinner.js https://raw.github.com/bseth99/jquery-ui/spinner-layout/ui/jquery.ui.spinner.js
+wget -O themes/base/jquery.ui.spinner.css https://raw.github.com/bseth99/jquery-ui/spinner-layout/themes/base/jquery.ui.spinner.css
